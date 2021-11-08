dafeadbd5e39e27c7fc326a82e25c16538fcc435
mamark90 <mamark90@gmail.com>
2021-11-08 16:40:16 -0500

    added packages and import packages in index.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch("http://localhost:3001/api/v1/categories/1/items/1", {
-      method: "GET",
-    }) 
-      .then(response => response.json())
-      .then(data => console.log(data))
+  // componentDidMount() {
+  //   fetch("http://localhost:3001/api/v1/categories/1/items/1", {
+  //     method: "GET",
+  //   }) 
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
 
-    }
+  //   }
 
   render() {
     return (
